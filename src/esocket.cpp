8efaf5868e727bd148996f16315c37c486e86f09
Brian Waters <brian.waters@sprint.com>
2019-07-05 16:53:05 +0000

    added Sprint copyright

--- src/esocket.cpp
+++ src/esocket.cpp
@@ -1,5 +1,6 @@
 /*
 * Copyright (c) 2009-2019 Brian Waters
+* Copyright (c) 2019 Sprint
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
